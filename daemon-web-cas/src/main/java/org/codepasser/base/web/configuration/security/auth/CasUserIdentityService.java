1d5606c78741d8f17a9061ba9db2143673e50ca5
Joker.cheng <codepasser@outlook.com>
2020-06-12 17:29:55 +0800

    daemon: fixed Create anonymous user when if you want users to have no access to 403.

--- daemon-web-cas/src/main/java/org/codepasser/base/web/configuration/security/auth/CasUserIdentityService.java
+++ daemon-web-cas/src/main/java/org/codepasser/base/web/configuration/security/auth/CasUserIdentityService.java
@@ -1,5 +1,7 @@
 package org.codepasser.base.web.configuration.security.auth;
 
+import org.codepasser.common.model.entity.inner.UserType;
+import org.codepasser.common.model.security.Authority;
 import org.codepasser.common.model.security.UserBasic;
 import org.codepasser.common.service.AuthorizationService;
 import org.codepasser.common.web.configuration.security.auth.UserIdentity;
@@ -8,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import java.util.EnumSet;
 import java.util.Optional;
 
 import static java.lang.String.format;
@@ -26,13 +29,13 @@ public class CasUserIdentityService implements UserDetailsService {
     }
     foundUser = authorizationService.loadUserByUsername(username);
     // Create anonymous user when if you want users to have no access to 403.
-    //    if (!foundUser.isPresent()) {
-    //      UserBasic anonymous = new UserBasic();
-    //      anonymous.setLocked(false);
-    //      anonymous.setAuthorities(EnumSet.noneOf(Authority.Role.class));
-    //      anonymous.setType(UserType.EXTERNAL);
-    //      foundUser = Optional.of(anonymous);
-    //    }
+    if (!foundUser.isPresent()) {
+      UserBasic anonymous = new UserBasic();
+      anonymous.setLocked(false);
+      anonymous.setAuthorities(EnumSet.noneOf(Authority.Role.class));
+      anonymous.setType(UserType.EXTERNAL);
+      foundUser = Optional.of(anonymous);
+    }
     if (!foundUser.isPresent()) {
       throw new UsernameNotFoundException(format("User [%s] not found.", username));
     }
